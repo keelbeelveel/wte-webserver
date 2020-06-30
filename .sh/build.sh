44d2429ff1ea0f60b723dbf0e27ab4830bbd4812
Kyle Belveal <k.belveal17@gmail.com>
2020-06-30 15:05:36 -0400

    fixed dummy name conflicts

--- .sh/build.sh
+++ .sh/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# script modified: Tue June 30, 2020 @ 02:09:49 EDT
-sudo cp wte.conf /etc/httpd/sites-available/wte-test.conf
+# script modified: Tue June 30, 2020 @ 03:04:21 EDT
+sudo cp wte.conf /etc/httpd/sites-available/wte.conf
 sudo cp index.html /var/www/wte-test.com/html/index.html
