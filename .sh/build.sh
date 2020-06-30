dcb22e33d2dcc2bb44e0e7e1f5ffa9637ffb1c15
Kyle Belveal <k.belveal17@gmail.com>
2020-06-30 13:59:37 -0400

    added sudo

--- .sh/build.sh
+++ .sh/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# script modified: Tue June 30, 2020 @ 01:30:31 EDT
-cp wte.conf /etc/httpd/sites-available/wte.conf
+# script modified: Tue June 30, 2020 @ 01:59:12 EDT
+sudo cp wte.conf /etc/httpd/sites-available/wte.conf
